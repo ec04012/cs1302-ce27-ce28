332aa17558df3a490dedeb7e7e6d1d242796f724
Wyatt Brooks <wdb59616@uga.edu>
2019-11-11 16:13:29 -0500

    added pom

---
+++ src/main/java/cs1302/sorting/SelectionSort.java
@@ -0,0 +1,8 @@
+package cs1302.sorting;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+	System.out.println("S E L E C T I O N S O R T");
+    } // main
+} // SelectionSort
