a51feb2faed091f4fa7f59af52e54dc8bd98d1fb
ec04012 <ec04012@uga.edu>
2019-11-11 16:16:00 -0500

    Added main method to BubbleSort.java

--- src/main/java/cs1302/sorting/BubbleSort.java
+++ src/main/java/cs1302/sorting/BubbleSort.java
@@ -1,5 +1,9 @@
 package cs1302.sorting;
 
-public class BubbleSort {
+/** Class to sort using BubbleSort. */
 
+public class BubbleSort {
+    public static void main (String[] args) {
+        System.out.println("Not yet implemented");
+    }
 }
