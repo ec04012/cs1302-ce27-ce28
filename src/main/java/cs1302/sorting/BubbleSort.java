769c0d07f791b65e96f06053f8160a634e6ab726
Okwuosar Nnyagu (O) <ocn63272@uga.edu>
2019-11-12 16:26:43 -0500

    Added the BubbleSort method and test cases

--- src/main/java/cs1302/sorting/BubbleSort.java
+++ src/main/java/cs1302/sorting/BubbleSort.java
@@ -8,23 +8,41 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main (String[] args) {
 	Integer[] yeah = {6,1,10,44,9,2};
-	System.out.println(Arrays.toString(yeah));
-	BubbleSort.bubble(yeah, 0, 5, Integer::compareTo);
-	System.out.println(Arrays.toString(yeah));
-	String[] ohYeah = {"asdf","e","ssf","","atatat"};
-	System.out.println(Arrays.toString(ohYeah));
-	BubbleSort.bubble(ohYeah, 2, 4, (o,t) -> o.length() - t.length());
-	System.out.println(Arrays.toString(ohYeah));
+	//System.out.println(Arrays.toString(yeah));
+	//BubbleSort.bubble(yeah, 0, 5, Integer::compareTo);
+	//System.out.println(Arrays.toString(yeah));
+	String[] ohYeah = {"asdf","ef","ssf","e","atatat"};
+	//System.out.println(Arrays.toString(ohYeah));
+	//BubbleSort.bubble(ohYeah, 2, 4, (o,t) -> o.length() - t.length());
+	//System.out.println(Arrays.toString(ohYeah));
+    System.out.println(Arrays.toString(yeah));
+    BubbleSort.bubbleSort(yeah , 0 , 5 , Integer::compareTo);
+    System.out.println(Arrays.toString(yeah));
+    System.out.println(Arrays.toString(ohYeah));
+    BubbleSort.bubbleSort(ohYeah , 0 , 4 ,(o , t) -> o.length() - t.length());
+    System.out.println(Arrays.toString(ohYeah));
+
     } // main
 
     public static <T> void bubble(T[] array, int lo, int hi, Comparator<T> c) {
 	for (int i = lo ; i < hi ; i ++) {
+
 	    if (c.compare(array[i],array[i + 1]) > 0) {
+
 		T temp = array[i];
 		array[i] = array[i + 1];
 		array[i + 1] = temp;
+
 	    } // if
 	} // for
     } // bubble
 
+    public static <T> void bubbleSort(T[] array , int lo , int hi , Comparator<T> c) {
+
+        for (int i = hi; i > lo;  i--) {
+
+            bubble(array , 0 , i , c);
+        }
+
+    }
 }
