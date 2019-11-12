20ef1dc13b49e223e24b14ede3516d07e7227eba
Wyatt Brooks <wdb59616@uga.edu>
2019-11-12 15:54:01 -0500

    added bubble method to BubbleSort

--- src/main/java/cs1302/sorting/BubbleSort.java
+++ src/main/java/cs1302/sorting/BubbleSort.java
@@ -1,9 +1,21 @@
 package cs1302.sorting;
 
+import java.util.Comparator;
+
 /** Class to sort using BubbleSort. */
 
 public class BubbleSort {
     public static void main (String[] args) {
         System.out.println("Not yet implemented");
     }
+
+    public static <T> void bubble(T[] array, int lo, int hi, Comparator<T> c) {
+	for (int i = lo ; i < hi ; i ++) {
+	    if (c.compare(array[i],array[i + 1]) > 0) {
+		T temp = array[i];
+		array[i] = array[i + 1];
+		array[i + 1] = temp;
+	    } // if
+	} // for
+    } // bubble
 }
