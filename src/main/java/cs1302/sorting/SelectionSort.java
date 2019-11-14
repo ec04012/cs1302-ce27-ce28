69de99670ba387197548160d0a38882f00e9994b
ec04012 <ec04012@uga.edu>
2019-11-14 16:11:01 -0500

    Implemented selectMin() in SelectionSort

--- src/main/java/cs1302/sorting/SelectionSort.java
+++ src/main/java/cs1302/sorting/SelectionSort.java
@@ -1,10 +1,40 @@
 package cs1302.sorting;
 
+import java.util.*;
+
 /** class to sort by selection*/
 
 public class SelectionSort {
 
     public static void main(String[] args) {
-	System.out.println("S E L E C T I O N S O R T");
+        Integer[] intArray = { 2, 3, 1, 4, 5};
+        String[]  strArray = {"wasd", "a", "fgyia", "cy", "bhy"};
+
+        System.out.println(Arrays.toString(intArray));
+        selectMin(intArray, 0, intArray.length - 1, Integer::compareTo);
+        System.out.println(Arrays.toString(intArray));
+
+        System.out.println("");
+
+        System.out.println(Arrays.toString(strArray));
+        selectMin(strArray, 2, strArray.length - 1, (a, b) -> a.length() - b.length());
+        System.out.println(Arrays.toString(strArray));
     } // main
+
+    public static <T> void selectMin(T[] array, int lo, int hi, Comparator<T> c) {
+        T temp;
+        int lowIndex = lo;
+
+        // Find the index of the lowest value
+        for (int i = lo; i < hi; i++) {
+            if (c.compare(array[lowIndex], array[i + 1]) > 0) {
+                lowIndex = i + 1;
+            }
+        }
+
+        // Swap first and lowest values
+        temp = array[lowIndex];
+        array[lowIndex] = array[lo];
+        array[lo] = temp;
+    }
 } // SelectionSort
