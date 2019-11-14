7448f54b1c1498783c373d36c519893a2ccd7e78
Okwuosar Nnyagu (O) <ocn63272@uga.edu>
2019-11-14 16:46:33 -0500

    Added incomplete partition method

--- src/main/java/cs1302/sorting/SelectionSort.java
+++ src/main/java/cs1302/sorting/SelectionSort.java
@@ -7,18 +7,22 @@ import java.util.*;
 public class SelectionSort {
 
     public static void main(String[] args) {
-        Integer[] intArray = { 2, 3, 1, 4, 5};
+        Integer[] array = { 1 , 3 , 2 ,4 ,5};
         String[]  strArray = {"wasd", "a", "fgyia", "cy", "bhy"};
 
-        System.out.println(Arrays.toString(intArray));
-        selectionSort(intArray, 0, intArray.length - 1, Integer::compareTo);
-        System.out.println(Arrays.toString(intArray));
+        //System.out.println(Arrays.toString(intArray));
+        //selectionSort(intArray, 0, intArray.length - 1, Integer::compareTo);
+        //System.out.println(Arrays.toString(intArray));
 
-        System.out.println("");
+        //System.out.println("");
 
-        System.out.println(Arrays.toString(strArray));
-        selectionSort(strArray, 0, 3, (a, b) -> a.length() - b.length());
-        System.out.println(Arrays.toString(strArray));
+        //System.out.println(Arrays.toString(strArray));
+        //selectionSort(strArray, 0, 3, (a, b) -> a.length() - b.length());
+        //System.out.println(Arrays.toString(strArray));
+        System.out.println(Arrays.toString(array)); // [ 1, 3, 2, 4, 5 ]
+        int newPivot = partition(array, 0, 2, 4, Integer::compareTo);
+        System.out.println(Arrays.toString(array)); // [ 1, 2, 5, 4, 3 ]
+        System.out.println(newPivot);
     } // main
 
     public static <T> void selectMin(T[] array, int lo, int hi, Comparator<T> c) {
@@ -43,5 +47,30 @@ public class SelectionSort {
 	    selectMin(array, i , hi , c);
 	} // for
     } // selectionSort
-    
+
+    public static <T> int partition(T[] array, int lo, int pivot, int hi, Comparator<T> c) {
+
+        T temp = array[pivot];
+        array[pivot] = array[hi];
+        array[hi] = temp;
+        int j = lo;
+        for (int i = lo; i < hi; i++) {
+
+            if(c.compare(array[i] , array[hi]) > 0) {
+
+                T temps = array[j];
+                array[j] = array[i];
+                array[i] = temps;
+                i = i + 1;
+
+            }
+        }
+            T tempa = array[j];
+            array[j] = array[hi];
+            array[hi] = tempa;
+            return j;
+
+
+
+    }
 } // SelectionSort
